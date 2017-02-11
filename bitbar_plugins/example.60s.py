f5aa7433d1b50c173347494bcc6c38d23c867f1d
damnever <dxc.wolf@gmail.com>
2017-02-11 14:29:39 +0800

    bitbar plugins

---
+++ bitbar_plugins/example.60s.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# <bitbar.title>Example</bitbar.title>
+# <bitbar.version>v0.1</bitbar.version>
+# <bitbar.author.github>damnever</bitbar.author.github>
+# <bitbar.author>damnever</bitbar.author>
+# <bitbar.desc>Example crontab.</bitbar.desc>
+# <bitbar.dependencies>python</bitbar.dependencies>
+
+
+if __name__ == '__main__':
+    print(':smile:')
+    print('---')
+    print(':point_right: ^_^ | color=green href=https://github.com/damnever')
