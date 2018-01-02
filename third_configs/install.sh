990831c23e6d947836a5d66ad27ad4ca2dcf7d3f
damnever <dxc.wolf@gmail.com>
2018-01-02 20:35:13 +0800

    Fix gdb issue for the macOS high sierra

--- third_configs/install.sh
+++ third_configs/install.sh
@@ -2,4 +2,10 @@
 
 cur_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-ln -vsfn $cur_dir/gdb-dashboard/.gdbinit $HOME/.gdbinit
+cp -vf $cur_dir/gdb-dashboard/.gdbinit $HOME/.gdbinit
+# For the macOS High Sierra..
+if [[ $(uname) == "Darwin" ]]; then  # Mac
+    fixstr="set startup-with-shell off"
+    # if [[ $(tail -n 1 $HOME/.gdbinit) != $fixstr ]]; then
+    echo $fixstr >> $HOME/.gdbinit
+fi
