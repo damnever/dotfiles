673fd5036c02924b58d027a1302f6e382dd6a374
XiaoChao Dong (@damnever) <dxc.wolf@gmail.com>
2020-02-19 09:38:09 +0800

    docker-clean

--- zsh_damnever/plugins/cleaner/cleaner.plugin.zsh
+++ zsh_damnever/plugins/cleaner/cleaner.plugin.zsh
@@ -1,5 +1,6 @@
 # Clean up dead docker containers and <none> tag images.
 docker-clean() {
     docker container rm $(docker container ls -f status=exited | awk '{if (NR > 1) print $1}') 2>/dev/null
-    docker rmi $(docker images | awk '{if (NR > 1 && $2 == "<none>") print $3}') 2>/dev/null
+    # docker rmi $(docker images | awk '{if (NR > 1 && $2 == "<none>") print $3}') 2>/dev/null
+    docker rmi $(docker images -f "dangling=true" -q)
 }
