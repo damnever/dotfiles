bca58163d7535609bef2e080c62df6b0d90557fa
Xiaochao Dong (@damnever) <the.xcdong@gmail.com>
2022-08-31 22:51:41 +0800

    F* fish, I want my bash/zsh back

---
+++ config/zsh/core/theme.zsh
@@ -0,0 +1,35 @@
+# Ref: https://github.com/ohmyzsh/ohmyzsh/blob/master/lib/theme-and-appearance.zsh
+autoload -U colors && colors
+
+export LSCOLORS="Gxfxcxdxbxegedabagacad"
+
+
+if [[ "$OSTYPE" == (darwin|freebsd)* ]]; then
+    # this is a good alias, it works by default just using $LSCOLORS
+    ls -G . &>/dev/null && alias ls='ls -G'
+
+    # only use coreutils ls if there is a dircolors customization present ($LS_COLORS or .dircolors file)
+    # otherwise, gls will use the default color scheme which is ugly af
+    [[ -n "$LS_COLORS" || -f "$HOME/.dircolors" ]] && gls --color -d . &>/dev/null && alias ls='gls --color=tty'
+else
+    # For GNU ls, we use the default ls color theme. They can later be overwritten by themes.
+    if [[ -z "$LS_COLORS" ]]; then
+      (( $+commands[dircolors] )) && eval "$(dircolors -b)"
+    fi
+
+    ls --color -d . &>/dev/null && alias ls='ls --color=tty' || { ls -G . &>/dev/null && alias ls='ls -G' }
+
+    # Take advantage of $LS_COLORS for completion as well.
+    zstyle ':completion:*' list-colors "${(s.:.)LS_COLORS}"
+fi
+
+# enable diff color if possible.
+if command diff --color /dev/null /dev/null &>/dev/null; then
+    alias diff='diff --color'
+fi
+
+setopt auto_cd
+setopt multios
+setopt prompt_subst
+
+[[ -n "$WINDOW" ]] && SCREEN_NO="%B$WINDOW%b " || SCREEN_NO=""
