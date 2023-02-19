0912a039475dbb8c42f8dff27a57e3d68778b92c
Xiaochao Dong (@damnever) <the.xcdong@gmail.com>
2023-02-19 22:02:10 +0800

    Oooooooooooooooooooooooooooooooooooooooooooooooooooooops
    Signed-off-by: Xiaochao Dong (@damnever) <the.xcdong@gmail.com>

--- install.sh
+++ install.sh
@@ -23,15 +23,12 @@ install_requirements_for_mac() {
     install_xcode_command_line_tools
     set -e
     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-    # for vim
+    # TODO: brew bundle --file=Brewfile
     brew install wget
-    brew install llvm # --with-toolchain
-    brew install shellcheck
+    brew install llvm
     brew install readline xz
-    # brew tap macvim-dev/macvim
-    # brew install --HEAD macvim-dev/macvim/macvim
-    brew install neovim
     brew install gnupg pinentry-mac
+    brew install neovim
     brew install zsh  # Default on macOS
     brew install ctags
     brew install fzf
@@ -40,23 +37,18 @@ install_requirements_for_mac() {
     brew install tmux reattach-to-user-namespace
     brew install coreutils findutils gnu-getopt
     brew ln gnu-getopt --force
-    # brew install yarn
-    brew install node
+    brew install node yarn  # Nodejs and..
     brew install watch
-    # brew install autojump
-    brew install go
-    brew install cloc
-    # brew install the_silver_searcher
-    brew install ripgrep fd bat
+    brew install go  # Golang
+    brew install ripgrep fd bat  # the_silver_searcher
     brew install tree
     brew install htop iftop
-    brew install polipo
     brew install fortune
     brew install direnv
     # fonts
-    brew tap homebrew/cask-fonts
+    # brew tap homebrew/cask-fonts
     # brew install font-ibm-plex --cask
-    brew install --cask font-jetbrains-mono-nerd-font # https://www.nerdfonts.com/
+    # brew install --cask font-jetbrains-mono-nerd-font # https://www.nerdfonts.com/
 }
 
 change_settings_for_mac() {
@@ -65,48 +57,31 @@ change_settings_for_mac() {
     # defaults write -g ApplePressAndHoldEnabled -bool false
 }
 
-install_requirements_for_ubuntu() {
-    set -e
-    sudo apt-get update
-    sudo apt-get install -y shellcheck clang-tidy
-    sudo apt-get install -y vim
-    sudo apt-get install -y zsh
-    sudo apt-get install -y tmux
-    sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev \
-        libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils
-    sudo apt-get install -y build-essential cmake python-dev ctags silversearcher-ag
-    git clone --depth 1 https://github.com/junegunn/fzf.git "$HOME/.fzf"
-    ~/.fzf/install
-    git clone https://github.com/yyuu/pyenv.git "$HOME/.pyenv"
-    git clone https://github.com/yyuu/pyenv-virtualenvwrapper.git "$HOME/.pyenv/plugins/pyenv-virtualenvwrapper"
-}
-
-
 install_prerequirements() {
-    if [[ "$platform" == "Darwin" ]]; then  # Mac
-        change_settings_for_mac
-        install_requirements_for_mac
-        grep -qxF "$(which zsh)" /etc/shells || echo "$(which zsh)" | sudo tee -a /etc/shells
-        chsh -s "$(which zsh)" #
-        sudo chsh -s "$(which zsh)" # Install for root as well.
-    elif [[ "$platform" == "Linux" ]]; then  # ubuntu for me
-        install_requirements_for_ubuntu
-        sudo chsh -s "$(which zsh)" ubuntu # vagrant
+    if [[ "$platform" != "Darwin" ]]; then  # Mac
+        echo "unsupported platform $platform"
+        exit 1
     fi
 
-    GLOBAL_PYTHON=3.7.0
+    change_settings_for_mac
+    install_requirements_for_mac
+    grep -qxF "$(which zsh)" /etc/shells || echo "$(which zsh)" | sudo tee -a /etc/shells
+    chsh -s "$(which zsh)" #
+    sudo chsh -s "$(which zsh)" # Install for root as well.
+
+    GLOBAL_PYTHON=3.11.0
     # https://github.com/pyenv/pyenv/issues/1219
     pyenv install $GLOBAL_PYTHON
     pyenv global $GLOBAL_PYTHON
     pip install ipython
 
-    # curl https://sh.rustup.rs -sSf | sh
-    # rustup toolchain add nightly
-    # rustup component add rust-src
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+    rustup toolchain add nightly
+    rustup component add rust-src
     # cargo +nightly install racer
 
     # Tmux
-    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+    # git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 }
 
 
@@ -134,18 +109,15 @@ setup_config_files() {
 
 setup_vim() {
     echo "-> setup vim .."
-    # pip install  flake8 pyflakes pep8  jedi pipenv vim-vint 'python-language-server[all]'
-    pip install pynvim yapf pylint
+    pip install pynvim yapf
     # go install golang.org/x/tools/cmd/benchstat@latest
     # go install golang.org/x/tools/cmd/godoc@latest
     # go install golang.org/x/tools/gopls@latest
-    # go install github.com/jstemmer/gotags@latest
-    # yarn global add bash-language-server
-    # npm install -g prettier bash-language-server
 
     # TODO: install lint and formaters for neovim null-ls?
 
     # Let's do it twice: https://github.com/wbthomason/packer.nvim
+    nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
     nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
 }
 
