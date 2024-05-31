694afb0514c1a28e4d511792cd22269ae36378fb
Xiaochao Dong (@damnever) <the.xcdong@gmail.com>
2024-05-31 23:51:57 +0800

    It's never too late to have a happy childhood.
    Signed-off-by: Xiaochao Dong (@damnever) <the.xcdong@gmail.com>

---
+++ config/nvim/lua/plugins/copilot_vim.lua
@@ -0,0 +1,26 @@
+local package = { -- For 'wbthomason/packer.nvim'
+    'github/copilot.vim',
+    event = { "BufReadPost" },
+}
+
+local config = function()
+    require('lib').vimbatch.global_vars({
+        copilot_no_tab_map = true,
+    })
+    require('lib').vimbatch.keymaps({
+        {
+            mode = 'i',
+            lhs = '<C-J>',
+            rhs = 'copilot#Accept("\\<CR>")',
+            opts = {
+                expr = true,
+                replace_keycodes = false
+            },
+        }
+    })
+end
+
+return {
+    package = package,
+    config = config,
+}
