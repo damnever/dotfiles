4c2d1c69510065bb525de2ee91a133cba3acbe41
Xiaochao Dong <the.xcdong@gmail.com>
2025-01-17 16:50:34 +0800

    God must have loved calories, she made so many of them.

---
+++ config/nvim/lua/plugins/conjure.lua
@@ -0,0 +1,12 @@
+local config = function()
+    require('lib').vimbatch.global_vars({
+        ["conjure#mapping#doc_word"] = false
+    })
+end
+
+return { {
+    "Olical/conjure",
+    ft = { "clojure", "janet", "python" }, -- etc
+    lazy = true,
+    config = config,
+} }
