22e298d8f3ae63bb71feb37a793eb1c64cdf36bf
Xiaochao Dong (@damnever) <the.xcdong@gmail.com>
2025-02-09 00:23:21 +0800

    ((lambda (foo) (bar foo)) (baz))
    Signed-off-by: Xiaochao Dong (@damnever) <the.xcdong@gmail.com>

--- config/nvim/lua/plugins/avante_nvim.lua
+++ config/nvim/lua/plugins/avante_nvim.lua
@@ -40,7 +40,7 @@ return { {
     lazy = false,
     version = false, -- Set this to "*" to always pull the latest release version, or set it to false to update to the latest code changes.
     config = config,
-    build = "make",  -- make BUILD_FROM_SOURCE=true
+    build = "make BUILD_FROM_SOURCE=true",
     dependencies = {
         "stevearc/dressing.nvim",
         "nvim-lua/plenary.nvim",
